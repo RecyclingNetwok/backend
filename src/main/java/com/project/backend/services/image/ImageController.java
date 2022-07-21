26d47f78b469c9a54b2a5474f4022e2d87bd10d9
ArklemX <avaikamarcel@gmail.com>
2022-07-21 16:12:09 +0200

    Add Image Service

--- src/main/java/com/project/backend/services/image/ImageController.java
+++ src/main/java/com/project/backend/services/image/ImageController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class ImageController {
 	@Autowired
 	ImageService imageService;
 
+	@PostMapping("/upload")
 	public ResponseEntity<?> uplaodImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
 		System.out.println("Original Image Byte Size - " + file.getBytes().length);
 		return imageService.uplaodImage(file);
